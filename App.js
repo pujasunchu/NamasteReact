2ad18a4cd7f21883ffccc54836a8f4f2c9b1a061
PUJA <pujasunchu4ever@gmail.com>
2024-10-13 12:55:22 +0530

    episode-01

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 /* <div id="parent">
     <div id="child">
         <h1></h1>
@@ -5,6 +7,37 @@
     </div>
 </div> */
 
-const heading = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "H1 tag"), React.createElement("h2", {}, "H2 tag")]));
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// const heading = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "H1 tag"), React.createElement("h2", {}, "H2 tag")]));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading); // this gets replaced because jsxheading render is after this render
+
+// //React Element
+// const JSXheading = <h1 id="heading">Heading from JSX</h1>;
+// root.render(JSXheading);
+
+// //React Functional Component
+
+// const CompHeading = () => {
+//     return <h1 id ="heading">Heading from Functional component</h1>
+// }
+
+// const number = 10000;
+
+// // Component Composition
+// const CompBody = () => {
+//     return <div>
+//         {number}
+//         {/* using a react element inside react component */}
+//         {JSXheading}
+//         <CompHeading />
+//         {/* Another way of calling a component<CompHeading></CompHeading> */}
+//         {/* Another way of calling a component as a function {CompHeading()} */}
+//         Component Body
+//     </div>
+// }
+
+// root.render(<CompBody />);
+
+// // another way of writing functional component without return statement
+// // const CompHeading = () => <h1 id ="heading">Heading from Functional component</h1>;
+// // root.render(<CompHeading />);
